bf9f4f61b7092d5b2be2fdc955f4eebeab87692f
saxiro <fabriciopbrb@gmail.com>
2019-11-22 16:02:36 -0300

    Início de React + JavaScript Avançado no Alura

---
+++ curso-react-alura/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import Tabela from './Tabela';
+
+function App() {
+  const autores = [
+    {
+      nome: 'Paulo', 
+      livro: 'React', 
+      preco:'1000'
+    },
+    {
+      nome: 'Daniel', 
+      livro: 'Java', 
+      preco:'99'
+    },
+    {
+      nome: 'Marcos', 
+      livro: 'Design', 
+      preco:'150'
+    },
+    {
+      nome: 'Bruno', 
+      livro: 'DevOps', 
+      preco:'100'
+    },
+    {
+      nome: 'Dave', 
+      livro: 'C#', 
+      preco:'90873'
+    }
+  ];
+  return (
+    <div className="App">
+      <Tabela autores = { autores }/>
+    </div>
+  );
+}
+
+export default App;
