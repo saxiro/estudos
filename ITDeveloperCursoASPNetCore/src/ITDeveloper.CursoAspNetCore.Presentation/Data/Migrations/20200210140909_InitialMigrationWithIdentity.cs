5904f6fbe166a6d261caf130199eae693850a299
saxiro <fabriciopbrb@gmail.com>
2020-02-10 19:01:42 -0300

    Estudos .NET CORE 3.1 no começo

---
+++ ITDeveloperCursoASPNetCore/src/ITDeveloper.CursoAspNetCore.Presentation/Data/Migrations/20200210140909_InitialMigrationWithIdentity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITDeveloper.CursoAspNetCore.Presentation.Data.Migrations
+{
+    public partial class InitialMigrationWithIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
