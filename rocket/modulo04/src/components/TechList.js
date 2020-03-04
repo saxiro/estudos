8689f2198101c404d6a69ddfbb0cfb9d59e44c79
saxiro <fabriciopbrb@gmail.com>
2020-03-03 23:18:24 -0300

    Arquivo que faltava

--- rocket/modulo04/src/components/TechList.js
+++ rocket/modulo04/src/components/TechList.js
@@ -35,7 +35,12 @@ class TechList extends Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <ul>
-                    {this.state.techs.map(tech => <TechItem key={tech} tech = {tech} onDelete={() => this.handleDelete(tech)} />)}
+                    {this.state.techs.map(tech => (
+                    <TechItem 
+                        key={tech} 
+                        tech = {tech} 
+                        onDelete={() => this.handleDelete(tech)} 
+                    />))}
                 </ul>
                 <input 
                     type="text" 
